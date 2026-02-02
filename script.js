51248becc192d9da9defbce59642357e39724734
rjneumeyer-source <rjneumeyer@gmail.com>
2026-02-02 17:29:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -117,7 +117,7 @@ function showMessage(response) {
 
     // Update the text content, display the message, and change the image to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = "See you on the 14th my princess";
+    yesMessage.textContent = "AHH BINKY AND DINKY ARE VALENTINE'S <3 !!!!";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/snoopywoodstockheart.gif";
